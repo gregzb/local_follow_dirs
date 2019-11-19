12c7c90b8f02e6571b7130ffb30d723d9fb72d63
Greg Zborovsky <greg.zbo3@gmail.com>
2019-11-18 23:16:58 -0500

    usr input

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 #include <string.h>
+
+#define MAX_DIR_SIZE 2048
 
 void print_size(int size) {
   double f_size = size;
@@ -23,10 +26,16 @@ int traverse_dir(char * dir_name, int level) {
   int total_size = 0;
   DIR * dir_stream = opendir(dir_name);
 
+  if (dir_stream == NULL) {
+    printf("Could not open %s\n", dir_name);
+    printf("Error: %s\n", strerror(errno));
+    return -1;
+  }
+
   struct dirent * file;
   while ((file = readdir(dir_stream)) != NULL) {
     struct stat fileStats;
-    char temp[1024];
+    char temp[MAX_DIR_SIZE];
     strcpy(temp, dir_name);
     strcat(temp, file->d_name);
     stat(temp, &fileStats);
@@ -55,12 +64,12 @@ int traverse_dir(char * dir_name, int level) {
 
     if ((fileStats.st_mode & S_IFDIR)) {
       if (strcmp(".", file->d_name) != 0 && strcmp("..", file->d_name) != 0) {
-        char new_path[1024];
-        strcpy(new_path, dir_name);
-        strcat(new_path, file->d_name);
-        strcat(new_path, "/");
+        // char new_path[MAX_DIR_SIZE];
+        // strcpy(new_path, dir_name);
+        // strcat(new_path, file->d_name);
+        strcat(temp, "/");
 
-        total_size += traverse_dir(new_path, level + 1);
+        total_size += traverse_dir(temp, level + 1);
       }
   	}
   }
@@ -69,12 +78,43 @@ int traverse_dir(char * dir_name, int level) {
   return total_size;
 }
 
-int main() {
-  char * dir_name = "./";
+int main(int argc, char ** argv) {
+  int max_dir_size = MAX_DIR_SIZE;
+  char dir_name[MAX_DIR_SIZE];
 
-  printf("Directory Name: %s\n", dir_name);
+  if (argc < 2) {
+    printf("No directory was found from the command line options.\n");
+    printf("Please enter one here: \n");
+    fgets(dir_name, MAX_DIR_SIZE, stdin);
+  } else {
+    strncpy(dir_name, argv[1], MAX_DIR_SIZE);
+  }
+
+  int dir_len = strlen(dir_name);
+
+  if (dir_len <= 1) {
+    printf("Input directory cannot be empty.\n");
+    return 1;
+  }
+
+  // https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+  if (dir_name[dir_len-1] == '\n') {
+    dir_name[dir_len-1] = '\0';
+  }
+
+  if (dir_name[dir_len-2] != '/') {
+    dir_name[dir_len-1] = '/';
+  }
+
+
+
+  printf("Root Directory Name: %s\n\n", dir_name);
 
   int total_size = traverse_dir(dir_name, 0);
+
+  if (total_size < 0) {
+    return 2;
+  }
 
   printf("Total Size: ");
   print_size(total_size);
